812c84ebe0526157ace9a58979ebaf8515fd72ea
Daniel Vieira <dann-vieira@hotmail.com>
2022-09-13 17:18:44 -0300

    feat: cria objeto para documentar os codigos de retorno das requisicoes http

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,11 @@ export type ProductBody = {
   name: string,
   amount: string,
 };
+
+export enum HttpStatusCode {
+  CREATED = 201,
+  OK = 200,
+  MISSING_FIELD = 400,
+  INVALID_CREDENTIALS = 401,
+  NOT_FOUND = 404,
+}
